5c9fce7abf25986c3b204ab5fc641407202e8d32
Fastrok <32041982+Fastrok@users.noreply.github.com>
2017-10-02 12:55:56 +0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,25 @@
+<?php
+$status = true;
+$x = 1;
+$y = 1;
+$userNumber = round(0.100);
+
+echo "Число ".$userNumber;
+
+while($status) {
+
+    if ($x > $userNumber) {
+        $status = false;
+        print_r('задуманное число не входит в чис ряд');
+    } else {
+        if ($x != $userNumber) {
+            $z = $x;
+            $x += $y;
+            $y = $z;
+        } else {
+            $status = false;
+            print_r('входит в числовой ряд');
+        }
+    }
+}
+?>
